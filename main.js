6c46f19fee34d947495284a026aa23d55d2c013b
socket1016 <socket1016@users.noreply.github.com>
2018-03-12 00:38:19 +0900

    no message

---
+++ main.js
@@ -0,0 +1,174 @@
+"use strict";
+var UI = /** @class */ (function () {
+    function UI() {
+    }
+    UI.updateLog = function (value) {
+        this.logElement.value += "\n" + value;
+    };
+    UI.updateAnswer = function (value) {
+        this.answerElement.innerText = value;
+    };
+    UI.removeLog = function () {
+        this.logElement.value = "";
+        this.answerElement.innerText = "";
+    };
+    UI.clearInput = function () {
+        this.numberInput.value = "123";
+        this.currentInput.value = "0";
+        this.existInput.value = "0";
+    };
+    UI.numberInput = document.querySelector("#number");
+    UI.currentInput = document.querySelector("#current");
+    UI.existInput = document.querySelector("#exist");
+    UI.culcButton = document.querySelector("#culculate");
+    UI.resetButton = document.querySelector("#reset");
+    UI.logElement = document.querySelector("#log");
+    UI.answerElement = document.querySelector("#expected-number");
+    return UI;
+}());
+var Judge = /** @class */ (function () {
+    function Judge(current, exist) {
+        if (current === void 0) { current = 0; }
+        if (exist === void 0) { exist = 0; }
+        this.current = 0;
+        this.exist = 0;
+        this.current = current;
+        this.exist = exist;
+    }
+    Judge.prototype.isEqual = function (judge) {
+        return this.current === judge.current &&
+            this.exist === judge.exist;
+    };
+    return Judge;
+}());
+var NumeronCommon = /** @class */ (function () {
+    function NumeronCommon() {
+    }
+    NumeronCommon.getJudge = function (answer, currentAnswer) {
+        var judge = new Judge();
+        answer.forEach(function (num, i) {
+            if (num === currentAnswer[i]) {
+                judge.current++;
+            }
+            else if (currentAnswer.some(function (current) { return current === num; })) {
+                judge.exist++;
+            }
+        });
+        return judge;
+    };
+    NumeronCommon.calcEntropy = function (answer, candicateNumbers) {
+        var _this = this;
+        var judgeList = candicateNumbers.map(function (candicateNums) {
+            return _this.getJudge(answer, candicateNums);
+        });
+        var uniques = [];
+        var _loop_1 = function (judge) {
+            var unique = uniques.find(function (u) { return u.judge.isEqual(judge); });
+            if (unique !== undefined) {
+                unique.count++;
+            }
+            else {
+                uniques.push({
+                    judge: judge,
+                    count: 1
+                });
+            }
+        };
+        for (var _i = 0, judgeList_1 = judgeList; _i < judgeList_1.length; _i++) {
+            var judge = judgeList_1[_i];
+            _loop_1(judge);
+        }
+        var counters = uniques.map(function (u) { return u.count; });
+        var pr = counters.map(function (count) { return count / candicateNumbers.length; });
+        var entropies = pr.map(function (p) { return p * -Math.log2(p); });
+        return entropies.reduce(function (prev, current) {
+            return prev + current;
+        });
+    };
+    NumeronCommon.createCandicate = function (numbers, length) {
+        var _this = this;
+        var result = [];
+        if (numbers.length < length) {
+            throw new Error();
+        }
+        else if (length === 1) {
+            for (var i = 0; i < numbers.length; i++) {
+                result[i] = [numbers[i]];
+            }
+        }
+        else {
+            numbers.forEach(function (number, i) {
+                var removedNumbers = Array.from(numbers);
+                removedNumbers.splice(i, 1);
+                var childResult = _this.createCandicate(removedNumbers, length - 1);
+                for (var _i = 0, childResult_1 = childResult; _i < childResult_1.length; _i++) {
+                    var child = childResult_1[_i];
+                    result.push([number].concat(child));
+                }
+            });
+        }
+        return result;
+    };
+    NumeronCommon.choice = function (array) {
+        return array[Math.floor(Math.random() * array.length)];
+    };
+    return NumeronCommon;
+}());
+var NumeronAI = /** @class */ (function () {
+    function NumeronAI() {
+        this._turn = 0;
+        this.candicate = NumeronCommon.createCandicate([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+        this.allCandicate = Array.from(this.candicate);
+    }
+    Object.defineProperty(NumeronAI.prototype, "turn", {
+        get: function () {
+            return this._turn;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    NumeronAI.prototype.updateCandicate = function (judge, callNumber) {
+        this._turn++;
+        this.candicate = this.candicate.filter(function (candicateNumber) {
+            var tmpJugde = NumeronCommon.getJudge(callNumber, candicateNumber);
+            return tmpJugde.isEqual(judge);
+        });
+    };
+    NumeronAI.prototype.decideCallNumber = function () {
+        var _this = this;
+        var entropies = this.candicate.map(function (numbers) { return NumeronCommon.calcEntropy(numbers, _this.candicate); });
+        var max = Math.max.apply(Math, entropies);
+        var index = entropies.findIndex(function (entropy) { return entropy === max; });
+        return this.candicate[index];
+    };
+    return NumeronAI;
+}());
+(function () {
+    var ai = new NumeronAI();
+    UI.culcButton.addEventListener("click", function () {
+        var numbers = UI.numberInput.value;
+        var numberArray = numbers.split("").map(function (str) { return Number(str); });
+        if (numberArray.length !== 3) {
+            UI.updateLog("3\u6841\u306E\u6570\u5B57\u3092\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044");
+            return;
+        }
+        var current = +UI.currentInput.value;
+        var exist = +UI.existInput.value;
+        var judge = new Judge(current, exist);
+        ai.updateCandicate(judge, numberArray);
+        var answer = ai.decideCallNumber();
+        if (answer === undefined) {
+            UI.updateLog("\u7B54\u3048\u304C\u5B58\u5728\u3057\u307E\u305B\u3093\u3002\u5165\u529B\u304C\u9593\u9055\u3048\u305F\u53EF\u80FD\u6027\u304C\u3042\u308A\u307E\u3059\u3002");
+            return;
+        }
+        UI.updateLog(ai.turn + "\u30BF\u30FC\u30F3\u76EE: \u6570\u5B57:" + numbers + ", \u3007: " + current + ", \u25B3: " + exist);
+        UI.updateAnswer("" + answer);
+        UI.clearInput();
+        UI.numberInput.value = answer.join("");
+    });
+    UI.resetButton.addEventListener("click", function () {
+        ai = new NumeronAI();
+        UI.clearInput();
+        UI.removeLog();
+    });
+})();
---
+++ main.ts
@@ -0,0 +1,168 @@
+
+class UI {
+    public static numberInput = document.querySelector("#number") as HTMLInputElement;
+    public static currentInput = document.querySelector("#current") as HTMLInputElement;
+    public static existInput = document.querySelector("#exist") as HTMLInputElement;
+    public static culcButton = document.querySelector("#culculate") as HTMLButtonElement;
+    public static resetButton = document.querySelector("#reset") as HTMLButtonElement;
+    public static logElement = document.querySelector("#log") as HTMLTextAreaElement;
+    public static answerElement = document.querySelector("#expected-number") as HTMLElement;
+
+    public static updateLog(value: string) {
+        this.logElement.value += "\n" + value;
+    }
+
+    public static updateAnswer(value: string) {
+        this.answerElement.innerText = value;
+    }
+
+    public static removeLog() {
+        this.logElement.value = "";
+        this.answerElement.innerText = "";
+    }
+
+    public static clearInput() {
+        this.numberInput.value = "123";
+        this.currentInput.value = "0";
+        this.existInput.value = "0";
+    }
+}
+
+class Judge {
+    public current: number = 0;
+    public exist: number = 0;
+    public constructor(current = 0, exist = 0) {
+        this.current = current;
+        this.exist = exist;
+    }
+    public isEqual(judge: Judge) {
+        return this.current === judge.current &&
+            this.exist === judge.exist;
+    }
+}
+
+class NumeronCommon {
+
+    public static getJudge(answer: number[], currentAnswer: number[]) {
+        const judge = new Judge();
+        answer.forEach((num, i) => {
+            if (num === currentAnswer[i]) {
+                judge.current++;
+            } else if (currentAnswer.some((current) => current === num)) {
+                judge.exist++;
+            }
+        });
+        return judge;
+    }
+
+    public static calcEntropy(answer: number[], candicateNumbers: number[][]) {
+        const judgeList = candicateNumbers.map((candicateNums) => {
+            return this.getJudge(answer, candicateNums);
+        });
+        const uniques: Array<{
+            judge: Judge;
+            count: number;
+        }> = [];
+        for (const judge of judgeList) {
+            const unique = uniques.find((u) => u.judge.isEqual(judge));
+            if (unique !== undefined) {
+                unique.count++;
+            } else {
+                uniques.push({
+                    judge: judge,
+                    count: 1
+                });
+            }
+        }
+        const counters = uniques.map((u) => u.count);
+        const pr = counters.map((count) => count / candicateNumbers.length);
+        const entropies = pr.map((p) => p * -Math.log2(p));
+        return entropies.reduce((prev, current) => {
+            return prev + current;
+        });
+    }
+
+    public static createCandicate(numbers: number[], length: number) {
+        const result: number[][] = [];
+        if (numbers.length < length) {
+            throw new Error();
+        } else if (length === 1) {
+            for (let i = 0; i < numbers.length; i++) {
+                result[i] = [numbers[i]];
+            }
+        } else {
+            numbers.forEach((number, i) => {
+                const removedNumbers = Array.from(numbers);
+                removedNumbers.splice(i, 1);
+                const childResult = this.createCandicate(removedNumbers, length - 1);
+                for (const child of childResult) {
+                    result.push([number, ...child]);
+                }
+            });
+        }
+        return result;
+    }
+
+    public static choice(array: any[]) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+}
+
+class NumeronAI {
+    private allCandicate: number[][];
+    private candicate: number[][];
+    private _turn: number = 0;
+    public get turn() {
+        return this._turn;
+    }
+    constructor() {
+        this.candicate = NumeronCommon.createCandicate([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+        this.allCandicate = Array.from(this.candicate);
+    }
+
+    public updateCandicate(judge: Judge, callNumber: number[]) {
+        this._turn ++;
+        this.candicate = this.candicate.filter((candicateNumber) => {
+            const tmpJugde = NumeronCommon.getJudge(callNumber, candicateNumber);
+            return tmpJugde.isEqual(judge);
+        });
+    }
+
+    public decideCallNumber() {
+        const entropies = this.candicate.map((numbers) => NumeronCommon.calcEntropy(numbers, this.candicate));
+        const max = Math.max(...entropies);
+        const index = entropies.findIndex(entropy => entropy === max);
+        return this.candicate[index];
+    }
+
+}
+
+(() => {
+    let ai = new NumeronAI();
+    UI.culcButton.addEventListener("click", () => {
+        const numbers = UI.numberInput.value;
+        const numberArray = numbers.split("").map((str) => Number(str));
+        if (numberArray.length !== 3) {
+            UI.updateLog(`3桁の数字を入力してください`);
+            return;
+        }
+        const current = +UI.currentInput.value;
+        const exist = +UI.existInput.value;
+        const judge = new Judge(current, exist);
+        ai.updateCandicate(judge, numberArray);
+        const answer = ai.decideCallNumber();
+        if (answer === undefined) {
+            UI.updateLog(`答えが存在しません。入力が間違えた可能性があります。`);
+            return;
+        }
+        UI.updateLog(`${ai.turn}ターン目: 数字:${numbers}, 〇: ${current}, △: ${exist}`);
+        UI.updateAnswer(`${answer}`);
+        UI.clearInput();
+        UI.numberInput.value = answer.join("");
+    });
+    UI.resetButton.addEventListener("click", () => {
+        ai = new NumeronAI();
+        UI.clearInput();
+        UI.removeLog();
+    });
+})();
